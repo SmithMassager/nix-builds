3d265da64ec4fd041f35f25b0af9e076e1874306
Ziwen Wang <ziwenwang@Ziwens-MacBook-Air.local>
2025-06-13 20:47:50 -0400

    update

--- shell.nix
+++ shell.nix
@@ -9,6 +9,7 @@ pkgs.mkShell {
     gmp
     flint
     openblas
+    pkgs.clang
   ];
 
   shellHook = ''
@@ -16,13 +17,16 @@ pkgs.mkShell {
     cd hnfproj
     FILE="defs.mk"
     MAPLE="/Library/Frameworks/Maple.framework/Versions/2025/"
+    PWD="$(pwd)/"
 
+    sed -i.bak "s|^\(export CC := \).*|\1clang|" "$FILE"
+    sed -i.bak "s|^\(export BASEDIR := \).*|\1$PWD|" "$FILE"
     sed -i.bak "s|^\(export MAPLEDIR := \).*|\1$MAPLE|" "$FILE"
     sed -i.bak "s|^\(export OPENBLAS := \).*|\1${openblas}|" "$FILE"
     sed -i.bak "s|^\(export GMP := \).*|\1${gmp}|" "$FILE"
     sed -i.bak "s|^\(export FLINT := \).*|\1${flint}|" "$FILE"
-    sed -i.bak "s|^\(export INSTALL_DIR := \).*|\1$out|" "$FILE"
+    #sed -i.bak "s|^\(export INSTALL_DIR := \).*|\1$out|" "$FILE"
 
-    make
+    #make
   '';
 }
