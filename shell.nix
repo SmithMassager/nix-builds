02c43f5ae074d68d26f4ee090a8b4655bbc4862a
Ziwen Wang <ziwenwang@Ziwens-MacBook-Air.local>
2025-06-26 22:21:25 -0400

    update

--- shell.nix
+++ shell.nix
@@ -13,8 +13,13 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    git clone https://github.com/deft-gur/hnfproj.git hnfproj
-    cd hnfproj
+    CSRC="SmithMassagerC"
+    MAPLESRC="SmithMassagerMaple"
+    rm -rf $CSRC
+    rm -rf $MAPLESRC
+    git clone https://github.com/SmithMassager/SmithMassagerC.git $CSRC
+    git clone https://github.com/SmithMassager/SmithMassagerMaple.git $MAPLESRC
+    cd SmithMassagerC
     FILE="defs.mk"
     MAPLE="/Library/Frameworks/Maple.framework/Versions/2025/"
     PWD="$(pwd)/"
@@ -25,8 +30,9 @@ pkgs.mkShell {
     sed -i.bak "s|^\(export OPENBLAS := \).*|\1${openblas}|" "$FILE"
     sed -i.bak "s|^\(export GMP := \).*|\1${gmp}|" "$FILE"
     sed -i.bak "s|^\(export FLINT := \).*|\1${flint}|" "$FILE"
-    #sed -i.bak "s|^\(export INSTALL_DIR := \).*|\1$out|" "$FILE"
+    sed -i.bak "s|^\(export INSTALL_DIR := \).*|\1../$MAPLESRC|" "$FILE"
 
-    #make
+    cd ..
+    echo "-------Environment ready. You can now run: cd SmithMassagerC && make && make install-------"
   '';
 }
