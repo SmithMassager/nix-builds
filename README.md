87b06b16acb147e66855069c41473b240cb8ad45
Ziwen Wang <ziwenwang@Ziwens-MacBook-Air.local>
2025-08-24 21:28:36 -0400

    Support building both C and Maple version

--- README.md
+++ README.md
@@ -2,7 +2,28 @@ Execute the following to fetch SmithMassagerMaple, SmithMassagerC, and to build 
 
 ```bash
 nix-shell
-make
 cd SmithMassagerC
+make
 make install
+```
+After the above command there should be a directory under
+`SmithMassagerC/install` which contains the shared `lib/` and `include/`
+files. In addition you should also see a `bin/`
+directory which contains the binary file `smith-massager`, this file simply
+takes a prime number, generate an interesting Smith form, and then executes
+Smith Massager algorithm. You can try executing it by,
+
+```bash
+./SmithMassagerC/install/bin/smith-massager 107
+```
+
+Now to build with maple support you must have already installed maple, an example of
+executing Smith Massager would be,
+```bash
+nix-shell --arg mapleDir "\"PATH_TO_MAPLE\""
+cd SmithMassagerC
+make
+make install
+cd ../SmithMassagerMaple
+maple SmithMassager/SmithMassager.mpl
 ```
--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, mapleDir ? null }:
 let
   gmp = pkgs.callPackage ./gmp.nix { };
   flint = pkgs.callPackage ./flint.nix { };
@@ -24,13 +24,28 @@ pkgs.mkShell {
     MAPLE="/Library/Frameworks/Maple.framework/Versions/Current/"
     PWD="$(pwd)/"
 
+    ${
+      if mapleDir != null then
+        ''
+        MAPLE = "${mapleDir}"
+        sed -i.bak "s|^\(export LDLIBS := \).*|\1-lopenblas -lgmp -lm -lmaplec -lflint|" "$FILE"
+        sed -i.bak "s|^\(export INSTALL_DIR := \).*|\1../$MAPLESRC|" "$FILE"
+        sed -i.bak "s|^\(export MAPLEDIR := \).*|\1$MAPLE|" "$FILE"
+        ''
+      else
+        ''
+        sed -i.bak "s|^\(export LDLIBS := \).*|\1-lopenblas -lgmp -lm -lflint|" "$FILE"
+        sed -i.bak "s|^\(export INSTALL_DIR := \).*|\1./install|" "$FILE"
+        sed -i.bak "/MAPLEDIR\|MAPLEBIN/d" "$FILE"
+        sed -i.bak '/^export MAPLEDIR := /d' "$FILE"
+        ''
+    }
+
     sed -i.bak "s|^\(export CC := \).*|\1clang|" "$FILE"
     sed -i.bak "s|^\(export BASEDIR := \).*|\1$PWD|" "$FILE"
-    sed -i.bak "s|^\(export MAPLEDIR := \).*|\1$MAPLE|" "$FILE"
     sed -i.bak "s|^\(export OPENBLAS := \).*|\1${openblas}|" "$FILE"
     sed -i.bak "s|^\(export GMP := \).*|\1${gmp}|" "$FILE"
     sed -i.bak "s|^\(export FLINT := \).*|\1${flint}|" "$FILE"
-    sed -i.bak "s|^\(export INSTALL_DIR := \).*|\1../$MAPLESRC|" "$FILE"
 
     cd ..
     echo "-------Environment ready. You can now run: cd SmithMassagerC && make && make install-------"
